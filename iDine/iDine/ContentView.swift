80de035329718b064efd7cdc259d1146484cb096
leonid nazarov <work.leonid@gmail.com>
2021-02-28 19:19:28 +0300

    add assets

---
+++ iDine/iDine/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Leonid on 28.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menu) { itemSection in
+                    Section(header: Text(itemSection.name)) {
+                        ForEach(itemSection.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Leonid on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
